57a896a78b17c97a8cb4167450b6dfbc09f6c1a6
Rhiannon Thompson <tommywoops@gmail.com>
2021-09-17 13:27:21 +0100

    Implemented image transitions

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,6 @@ import {Switch, Route} from "react-router-dom"
 import HomePageLayout from "./layouts/HomePageLayout";
 import HomePage from "./pages/HomePage";
 
-//todo finish the homepage info section and services offered section
-//todo create a contact form and add it to the top of the footer
-//todo create copy data file and pass all copy in as props
-//todo organize tailwind to use @apply for global styles etc
-//todo fix all links
-// End of tomorrow
-//todo create the contact page 
-//todo create the about page
-// End of wednesday
-//todo create the services page 
-//todo nice to have - a gallery page - if I get time 
-// End of thursday
-
 export default function App() {
   return (
     <Switch>
