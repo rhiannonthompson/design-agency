8bb14eb6c98591ab3923228c58235e13826c1e22
Rhiannon Thompson <tommywoops@gmail.com>
2021-10-15 22:41:40 +0100

    Style single service

--- src/pages/SingleServicePage.jsx
+++ src/pages/SingleServicePage.jsx
@@ -9,16 +9,19 @@ export default function SingleServicePage({ ...props }) {
   const id = parseInt(location.pathname.slice(location.pathname.length - 1));
 
   function selectImage(id) {
-    const matched = POST_CONTENT.filter(item => item.id === id);
-    const matchedImage = matched[0]
+    const matched = POST_CONTENT.filter((item) => item.id === id);
+    const matchedImage = matched[0];
     return matchedImage.image;
   }
 
-
   return (
-    <Section heading={`Service ${id}`} paddingBottom={"my-10 md:my-20"}>
-      <img src={selectImage(id)} alt="Service description"/>
-      <ServiceInfo />
+    <Section heading={`Service ${id}`} paddingBottom={"my-12 md:my-20"}>
+      <div className="lg:flex lg:justify-between">
+        <div className="mr-10 w-full">
+          <img className="rounded-sm" src={selectImage(id)} alt="Service description"/>
+        </div>
+        <ServiceInfo />
+      </div>
     </Section>
   );
 }
